4292b526850de239750d05fd593ec91879dce35a
Lawrence <lhshein14@gmail.com>
2024-02-24 18:52:06 +0800

    navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,10 @@
 const NavBar = () => {
   return (
-    <div className="flex flex-rows px-2 py-2 bg-gray-500">
-      <img src="/vite.svg" />
+    <div className="flex flex-rows px-2 py-2 items-center">
+      <img
+        src="/vite.svg"
+        className="w-14 h-14 bg-white shadow-lg rounded-xl p-1"
+      />
       <div className="ml-3 text-xl font-bold">Game Hub</div>
     </div>
   );
