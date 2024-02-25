9679a2841a3f8538f2f6a2c9274a441c57f7d28f
Lawrence <lhshein14@gmail.com>
2024-02-25 23:35:17 +0800

    api

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,10 @@
 import axios from "axios"
+require('dotenv').config({ path: '.evn' })
 export default axios.create({
     baseURL: 'https://free-to-play-games-database.p.rapidapi.com/api',
     headers: {
-        'X-RapidAPI-Key': '78d4a2f4f6msh6a015dfb10f3defp14ff69jsnc9c64adef827',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+      'X-RapidAPI-Key': '78d4a2f4f6msh6a015dfb10f3defp14ff69jsnc9c64adef827',
+      'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
       }
     
     
