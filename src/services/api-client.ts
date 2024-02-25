3235f795a6a0a013557961eef320f8d6f3ef9226
Lin Htet Shein <127378804+Lawrecesss@users.noreply.github.com>
2024-02-25 21:47:37 +0800

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://rawg-video-games-database.p.rapidapi.com',
+    baseURL: 
     headers: {
-        'X-RapidAPI-Key': '78d4a2f4f6msh6a015dfb10f3defp14ff69jsnc9c64adef827',
-        'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+        
       }
     
 }
